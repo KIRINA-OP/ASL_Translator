3d82211db9e0188e87b02046e872c09bab416f11
Kirina <haojia_kirina@outlook.com>
2023-12-23 21:43:33 -0500

    able to boot

--- firmwareDeploy.sh
+++ firmwareDeploy.sh
@@ -2,6 +2,10 @@
 
 #flash linux kernel, same as that one
 
+
 #tar the emdebian file and flash it to the tf card
-tar -cvf target-rootfs.tar emdebian/mindb/target-rootfs
-tar xvf target-rootfs.tar -C /media/stephen/rootfs
+tar -cvf target-rootfs.tar emdebian/mindb/target-rootfs/
+tar -xvf target-rootfs.tar -C /media/stephen/rootfs
+mv /media/stephen/rootfs/emdebian/mindb/target-rootfs/* /media/stephen/rootfs/
+rm -rf /media/stephen/rootfs/emdebian
+rm target-rootfs.tar
