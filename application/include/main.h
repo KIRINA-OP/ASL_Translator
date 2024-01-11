3744e6205545d4fc16db1c7d02450f4cab04be75
Kirina <haojia_kirina@outlook.com>
2024-01-10 23:39:19 -0500

    config class structure update

--- application/include/main.h
+++ application/include/main.h
@@ -1,4 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <iostream>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string>
+
+
+#define LOCK pthread_mutex_t 
 
 void helloworld();
+
+#endif
