dc23fc94d69196cae3cf27246741754d8d9d27bd
Kirina <haojia_kirina@outlook.com>
2024-03-06 23:52:21 -0500

    gpio control draft

--- application/include/visControl.h
+++ application/include/visControl.h
@@ -1,12 +1,40 @@
 #ifndef VISCONTROL_H
 #define VISCONTROL_H
-#include "main.h"
+
+
 #include <vector>
-#include "visGui.h"
 #include "visConfig.h"
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string>
+#include <stdint.h>
+#include <cstring>
+#include <cstdio>
+#include <mutex>
+#include <unistd.h>
+#include "gpiod/include/gpiod.h"
+#include <unordered_map>
+
+
+// #include <sys/mman.h>
+
+#define UBYTE   uint8_t
+#define UWORD   uint16_t
+#define UDOUBLE uint32_t
+
+//define specific pins
+
+#define OLED_CS         8		
+#define OLED_RST        27	
+#define OLED_DC         25	
+
+const unsigned int M_SECOND = 1000;
+
 class visControl{
     protected:
-    
+    std::mutex lock;
     public:
 
 };
@@ -33,26 +61,38 @@ class visCameraControl: public visControl{
 };
 
 //gpio polling by starting a pthread from thread pool
-class visDeviceControl: public visControl{
-    //gpio Control object
-    //guiMessageQueue  
-    class GPIO{};
-    std::vector<GPIO*> gpios;
-    visGui * gui; //take the controller pointer as the parameter, and add task to it
+class visGpioControl: public visControl{
+    std::string dev_name;
+
+    gpiod_chip *chip;
+    // GPIO_REGISTER * gpio_base;
+    std::unordered_map<UWORD, gpiod_line*> pin_read;
+    std::unordered_map<UWORD, gpiod_line*> pin_write;
+    bool configPin(UWORD pin, bool write);
     public:
-    //polling from outside
-    visDeviceControl(visGui* gui);
-    void readGpio();
-    void writeGpio();
-    ~visDeviceControl();
+    visGpioControl();
+    bool init();
+    UBYTE readPin(UWORD pin);
+    void writePin(UWORD pin, UBYTE Value);
+    ~visGpioControl();
 };
 
 //TODO: implement in screen control
 class visScreenControl: public visControl{
-    visGui* gui;
+    void oled_reset();
+    visGpioControl * gpio;
     public:
-    visScreenControl(visGui* gui);
+    visScreenControl(visGpioControl* gpio);
+    bool init();
+    void screenClear();
+    void paintNewImage();
+    void paintSelectImage();
+    void disPlay();
+
+    bool acquireLock();
     ~visScreenControl();
 };
+
+
 
 #endif
---
+++ application/source/visControl.cpp
@@ -0,0 +1,94 @@
+#include "visControl.h"
+#include "gpiod/include/gpiod.h"
+#include <stdexcept>
+#include <string>
+
+//first try: using libgpiod.h in C
+
+visGpioControl::visGpioControl(){
+
+}
+
+bool visGpioControl::init(){
+    chip = gpiod_chip_open("/dev/gpiochip0");
+    if(!chip){
+        throw std::runtime_error("unable to get the gpio controller");
+        return false;
+    }
+    return true;
+}
+
+bool visGpioControl::configPin(UWORD pin, bool write){
+    static int n = 0;
+    if(write){
+        if(pin_write.find(pin) != pin_write.end())
+            return true;
+        if(pin_read.find(pin) != pin_read.end())
+            pin_read.erase(pin);
+        gpiod_line* target_pin = gpiod_chip_get_line(chip, pin);
+        gpiod_line_request_output(target_pin, ("w" + std::to_string(n)).c_str(), 0);
+        if(!target_pin)
+            return false;
+        pin_write[pin] = target_pin;
+    }
+    else{
+        if(pin_write.find(pin) != pin_write.end())
+            return true;
+        if(pin_read.find(pin) != pin_read.end())
+            pin_read.erase(pin);
+        gpiod_line* target_pin = gpiod_chip_get_line(chip, pin);
+        gpiod_line_request_output(target_pin, ("w" + std::to_string(n)).c_str(), 0);
+        if(!target_pin)
+            return false;
+        pin_write[pin] = target_pin;
+    }
+    return true;
+}
+
+UBYTE visGpioControl::readPin(UWORD pin){
+    if(pin_read.find(pin) == pin_read.end())
+        configPin(pin, false);
+    gpiod_line* target_pin = pin_read[pin];
+    int ret = gpiod_line_get_value(target_pin);
+    if(ret == -1)
+        throw std::runtime_error("invalid read operation");
+    return (UBYTE)ret;
+}
+
+void visGpioControl::writePin(UWORD pin, UBYTE val){
+    if(pin_write.find(pin) == pin_write.end())
+        configPin(pin, false);
+    gpiod_line* target_pin = pin_write[pin];
+    int ret = gpiod_line_set_value(target_pin, val);
+    if(ret == -1)
+        throw std::runtime_error("invalid read operation");
+}
+
+
+visGpioControl::~visGpioControl(){
+    gpiod_chip_close(chip);
+}
+
+
+visScreenControl::visScreenControl(visGpioControl * gpio){
+    
+}
+
+void visScreenControl::oled_reset(){
+    gpio->writePin(OLED_RST, 1); //oled rst, 1
+    //delay 100ms
+    usleep(100 * M_SECOND);
+    gpio->writePin(OLED_RST, 0); //oled rst, 1
+    usleep(100 * M_SECOND);
+    gpio->writePin(OLED_RST, 1); //oled rst, 1
+    usleep(100 * M_SECOND);
+}
+
+
+bool visScreenControl::init(){
+    //reset
+
+    //init register
+
+    //turn_on writeReg
+}
